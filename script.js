9e77a6c29246b9bc26fdf2e144cfc8eb28bb7ff5
falovic <mail@falovic.com>
2022-01-31 06:21:55 +0300

    update

--- script.js
+++ script.js
@@ -918,3 +918,50 @@ emeklilikYasi(1997, "Fatih");
 emeklilikYasi(2000, "Ceren");
 emeklilikYasi(1991, "Deniz");
 emeklilikYasi(1940, "Rıfat");
+
+// function declarations
+
+function sum(a, b) {
+   var c = a + b;
+   return c;
+}
+
+console.log(sum(10, 20))
+
+// function expressions
+
+const sum2 = function (a, b) {
+   var c = a + b;
+   return c;
+}
+
+console.log(sum2(10, 30))
+console.log(sum2(10)) // NaN
+console.log(sum2(10, 30, 40, 50)) // fazla olan değerler gözardı edilir.
+
+
+val = "NaN değeri almaması için default parametre ataması yapılabilir."
+
+const sum3 = function (a, b) {
+   if (typeof a === "undefined") { a = 0; }
+   if (typeof b === "undefined") { b = 0; }
+   var c = a + b;
+   return c;
+}
+
+val = "ES6 (yeni gelen pratik default atama özelliği)"
+
+const sum4 = function (a = 0, b = 0) {
+   var c = a + b;
+   return c;
+}
+
+function sumAll() {
+   var total = 0;
+   for (let i = 0; i < arguments.length; i++) {
+      total += arguments[i];
+   }
+   return total;
+}
+
+console.log(sumAll(10, 20, 30, 40));
